8c812e31f332f2f6de2edbb24b507a59ef1cad98
mackan22 <markkus.andersson@gmail.com>
2025-09-29 15:43:27 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,26 @@
+def double(n):
+    return 2*n
+
+def triple(n):
+    return 3*n
+
+def quadruple(n):
+    return double(double(n))
+
+def funky(n, m):
+    return triple(n) + quadruple(m)
+
+a = 3
+b = 14
+
+d1 = double(a)       # 6
+d2 = double(b)       # 28
+
+t1 = triple(a)       # 9
+t2 = triple(b)       # 42
+
+q1 = quadruple(a)    # 12
+q2 = quadruple(b)    # 56
+
+f1 = funky(a, b)     # 65
+f2 = funky(b, b)     # 98
